e0837b70e943a4cee5fef055aa6db7c846713e29
Francisco Solé <franciscosoleescande@gmail.com>
2025-09-02 01:47:26 -0300

    refactor(Auth): add otp token expiration check when login

--- services/AuthService.py
+++ services/AuthService.py
@@ -3,7 +3,7 @@ from flask import g
 from configs.ServerConfig import logger
 
 from utils import UsersUtils
-from managers import AuthManager
+from managers import AuthManager, OtpManager
 from configs import AuthConfig
 
 def login(model, request_id):
@@ -50,7 +50,7 @@ def login_otp(model, request_id):
     user_id = model["user_id"]
     otp_token = model["otp_token"]
 
-    logger.info(f"{request_id} - '{user_id}' is trying to login")
+    logger.info(f"{request_id} - user_id '{user_id}' is trying to login with otp '{otp_token}'...")
     result = AuthManager.login_otp(user_id=user_id,
                                    otp_token=otp_token,
                                    request_id=request_id)
@@ -65,6 +65,22 @@ def login_otp(model, request_id):
         logger.critical(f"{request_id} - invalid token or user_id")
         return {"code": "0204", "description": AuthConfig.login_otp_code_map["0204"]}, 204
 
+    if result["data"]["expires_at"] < datetime.datetime.now():
+        logger.info(f"{request_id} - user's otp_token is expired")
+        g.response_code = "0410"
+        return {"code": "0410", "description": AuthConfig.login_otp_code_map["0410"]}, 400
+
+    logger.info(f"{request_id} - user's otp_token is valid, deleting used otp...")
+    deleted = OtpManager.delete_otp(user_id=user_id,
+                                    otp_token=otp_token,
+                                    request_id=request_id)
+
+    if not deleted["ok"]:
+        logger.critical(f"{request_id} - database failed when deleting used otp_token")
+        g.response_code = "0501"
+        return {"code": "0501", "description": AuthConfig.login_otp_code_map["0501"]}, 500
+
+    logger.info(f"{request_id} - user logged in successfully")
     g.response_code = "0200"
     return {
         "code": "0200",
