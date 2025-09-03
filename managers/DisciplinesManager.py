064f6b6eb3cc71758a54f11b99c3ec8455c72010
Francisco Solé <franciscosoleescande@gmail.com>
2025-09-03 01:12:19 -0300

    feat(DisciplinesManager): first version

---
+++ managers/DisciplinesManager.py
@@ -0,0 +1,21 @@
+from configs.ServerConfig import logger
+from utils.DatabaseUtils import with_db_connection
+
+@with_db_connection
+def does_disciplines_exist(final_response, conn, cursor, discipline_id, request_id):
+    try:
+        query = """
+            SELECT 1
+            FROM disciplines
+            WHERE id = %s
+            LIMIT 1;
+        """
+        values = (discipline_id,)
+
+        cursor.execute(query, values)
+        final_response["data"] = cursor.fetchone()
+    except:
+        logger.exception(f"{request_id} - an error occurred while trying to check if discipline exists")
+        final_response["ok"] = False
+
+    return final_response
