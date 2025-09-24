f27b13f9f4f57f181613ddc6dd9db820ee5bc230
Luciana Bermudez <bermudezluciana@hotmail.com>
2025-09-23 23:24:01 -0300

    move to authmodel

--- models/AuthModel.py
+++ models/AuthModel.py
@@ -9,3 +9,16 @@ class login_otp(Schema):
 
 class refresh_token(Schema):
     jwt_token = fields.String(required=True, validate=validate.Length(min=1))
+
+class verify_otp(Schema):
+    username = fields.String(required=True, validate=validate.Length(min=1, max=50))
+    verification_code = fields.String(required=True, validate=validate.Length(min=1, max=6))
+
+class resend_otp(Schema):
+    username = fields.String(required=True, validate=validate.Length(min=1, max=50))
+    type = fields.String(required=True, validate=validate.OneOf(['REGISTRATION', 'RECOVERY']))
+
+class reset_password(Schema):
+    reset_token = fields.String(required=True, validate=validate.Length(min=1))
+    new_password = fields.String(required=True, validate=validate.Length(min=6, max=100))
+
