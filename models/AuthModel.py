3821cee15f497fdbeed1f0a7fd008488147046b0
Luciana Bermudez <bermudezluciana@hotmail.com>
2025-09-23 21:14:56 -0300

    Update AuthModel.py

--- models/AuthModel.py
+++ models/AuthModel.py
@@ -9,8 +9,3 @@ class login_otp(Schema):
 
 class refresh_token(Schema):
     jwt_token = fields.String(required=True, validate=validate.Length(min=1))
-
-class recover_account(Schema):
-    username = fields.String(required=True, validate=validate.Length(min=1, max=50))
-    new_password = fields.String(required=True, validate=validate.Length(min=1, max=50))
-    otp_token = fields.String(required=True, validate=validate.Length(min=1, max=6))
