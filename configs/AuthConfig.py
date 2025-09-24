8a833689ef495d03618d55f42888bc165de3c1cf
Luciana Bermudez <bermudezluciana@hotmail.com>
2025-09-23 22:16:52 -0300

    Update AuthConfig.py

--- configs/AuthConfig.py
+++ configs/AuthConfig.py
@@ -31,3 +31,32 @@ refresh_token_code_map = {
     "0411": ("requested jwt token is invalid", 400),
     "0500": ("the request could not be processed", 500),
 }
+
+verify_otp_code_map = {
+    "0200": ("ok", 200),
+    "0400": ("bad request", 400),
+    "0404": ("invalid verification code", 404),
+    "0410": ("verification code expired", 400),
+    "0500": ("the request could not be processed", 500),
+    "0501": ("the request could not be processed", 500)
+}
+
+resend_otp_code_map = {
+    "0200": ("ok", 200),
+    "0400": ("bad request", 400),
+    "0404": ("username not found", 404),
+    "0411": ("account already verified or invalid request", 400),
+    "0412": ("account not verified, cannot request recovery", 400),
+    "0500": ("the request could not be processed", 500),
+    "0501": ("the request could not be processed", 500),
+    "0502": ("the request could not be processed", 500)
+}
+
+reset_password_code_map = {
+    "0200": ("ok", 200),
+    "0400": ("bad request", 400),
+    "0413": ("invalid reset token", 401),
+    "0414": ("reset token expired", 401),
+    "0500": ("the request could not be processed", 500),
+    "0501": ("the request could not be processed", 500)
+}
