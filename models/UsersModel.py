62b92722b5319ef6f38d35e69a73ec68ea25c509
Luciana Bermudez <bermudezluciana@hotmail.com>
2025-09-23 21:15:18 -0300

    Update UsersModel.py

--- models/UsersModel.py
+++ models/UsersModel.py
@@ -13,3 +13,11 @@ class update_user_information(Schema):
     last_name = fields.String(required=False)
     telephone = fields.String(required=False)
     contact_email = fields.String(required=False)
+
+class resend_otp(Schema):
+    username = fields.String(required=True, validate=validate.Length(min=1, max=50))
+    type = fields.String(required=True, validate=validate.OneOf(['REGISTRATION', 'RECOVERY']))
+
+class reset_password(Schema):
+    reset_token = fields.String(required=True, validate=validate.Length(min=1))
+    new_password = fields.String(required=True, validate=validate.Length(min=6, max=100))
